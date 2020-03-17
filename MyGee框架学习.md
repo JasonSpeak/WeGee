233e926bf0f4d2320ae7b508a11d6ae330e7919e
jasonspeak <1208694121@qq.com>
2020-03-17 14:12:47 +0800

    init mod

---
+++ MyGee框架学习.md
@@ -0,0 +1,36 @@
+## Gee框架
+
+### 什么是web框架
+
+#### 为什么要用框架
+- 各种语言都会自带一些基础的web功能，例如端口监听，映射静态路由，解析http报文等
+- 但是相对于实际web开发中，基础web功能缺少了：
+    - 动态路由：例如hello/:name，hello/*这类的规则。
+    - 鉴权：没有分组/统一鉴权的能力，需要在每个路由映射的handler中实现。
+    - 模板：没有统一简化的HTML机制。
+    - ...
+- 总而言之，就是语言自带的web功能不够灵活，使用起来不够简洁，或者不能满足一些实际的业务需求
+
+#### 框架的功能
+- 路由(Routing)：将请求映射到函数，支持动态路由。例如'/hello/:name。
+- 模板(Templates)：使用内置模板引擎提供模板渲染机制。
+- 工具集(Utilites)：提供对 cookies，headers 等处理机制。
+- 插件(Plugin)：本身功能有限，但提供了插件机制。可以选择安装到全局，也可以只针对某几个路由生效。
+
+
+### Day1 搭建Gee基本框架
+#### 实现http.Handler接口
+- Go启动web监听的方法是这样的
+```golang
+http.ListenAndServe(address string,h handler)
+```
+- 其中，`address`是监听的地址，而`handler`是访问该地址时运行的实例
+- 而`handler`是一个接口，需要实现方法`ServeHTTP(w ResponseWriter,r *Request)`
+- 所以只要传入任何实现了`ServeHTTP`接口的实例，所有的http请求就都交给了该实例处理
+
+
+### Day2 实现上下文Context
+#### 为什要使用上下文Context
+- 对Web服务来说，无非是根据请求*http.Request，构造响应http.ResponseWriter。但是这两个对象提供的接口粒度太细，比如我们要构造一个完整的响应，需要考虑消息头(Header)和消息体(Body)，而 Header 包含了状态码(StatusCode)，消息类型(ContentType)等几乎每次请求都需要设置的信息。因此，如果不进行有效的封装，那么框架的用户将需要写大量重复，繁杂的代码，而且容易出错。针对常用场景，能够高效地构造出 HTTP 响应是一个好的框架必须考虑的点
+- 针对使用场景，封装*http.Request和http.ResponseWriter的方法，简化相关接口的调用，只是设计 Context 的原因之一。对于框架来说，还需要支撑额外的功能。例如，将来解析动态路由/hello/:name，参数:name的值放在哪呢？再比如，框架需要支持中间件，那中间件产生的信息放在哪呢？Context 随着每一个请求的出现而产生，请求的结束而销毁，和当前请求强相关的信息都应由 Context 承载。因此，设计 Context 结构，扩展性和复杂性留在了内部，而对外简化了接口。路由的处理函数，以及将要实现的中间件，参数都统一使用 Context 实例， Context 就像一次会话的百宝箱，可以找到任何东西
+
---
+++ gee/context.go
@@ -0,0 +1,82 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+//H defines a nick name for Json data
+type H map[string]interface{}
+
+//Context defines a data struct for gee http link
+type Context struct {
+	//http origin object
+	Writer http.ResponseWriter
+	Req    *http.Request
+	//request info
+	Path   string
+	Method string
+	//response info
+	StatusCode int
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+//PostForm used to get values from forms
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+//Query used to get values from url query
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+//Status used to set Status of current Context
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+//SetHeader used to set current context's header
+func (c *Context) SetHeader(key, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+//String used to return  string format of values
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+//JSON used to encode obj to json type
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+//Data used to set data into current context
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+//HTML used to set html into current context
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
